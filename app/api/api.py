7ab7a957c894f192956ace9e07cda6ce9855d161
Marouen Marzouki <marouen.marzouki@gmail.com>
2020-10-17 00:17:05 -0500

    forgot to update these

--- app/api/api.py
+++ app/api/api.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from flask import jsonify
 
@@ -7,4 +8,7 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "Hello World!"})
 
-
+# start the app
+if '__main__' == __name__:
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(debug=True, port=8000)
