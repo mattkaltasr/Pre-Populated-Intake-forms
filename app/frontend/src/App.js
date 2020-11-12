7b0f5113095b4c525e4ff93cc5c856e32b88f57d
darch3 <darch3@gatech.edu>
2020-11-11 19:27:36 -0600

    Add styling changes

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -3,10 +3,14 @@ import React, { useEffect, useState, useRef } from "react";
 import PatientInfo from "./components/forms/PatientInfo";
 import { loadPatientById } from "./util/apiHelpers";
 
+import FormContainer from "./components/containers/FormContainer";
+import FormLabel from "./components/formElements/FormLabel";
+
+import "./components/formElements/TextInput.css";
+
 import "./App.css";
 
-// fritz doyle
-// const TEST_PATIENT_ID = "fb7a640d-1f8e-4320-9e07-20f27f8e18f2";
+// fritz doyle example patientId "fb7a640d-1f8e-4320-9e07-20f27f8e18f2";
 
 function App() {
   const [error, setError] = useState(null);
@@ -37,20 +41,37 @@ function App() {
         setLoading,
       });
     }
+
+    if (error) {
+      alert('PatientId does not exist.')
+    }
   }, [patientId]);
 
   return (
-    <div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            ref={inputRef}
-            value={value}
-            onChange={e => setValue(e.target.value)}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+    <div className="app-container">
+      <FormContainer
+      title="Select a Patient"
+      key="form-group-0"
+      renderFormComponents={() => (
+        <div className="flex" style={{ flex: 1, flexWrap: "wrap" }}>
+          <div className="flex flex-col" style={{ flex: 1 }}>
+            <div className="flex" style={{ flexWrap: "wrap" }}>
+              <FormLabel title="Patient ID" />
+                <form onSubmit={handleSubmit}>
+                  <input
+                    style={{ width: "100%", margin: "0px 5px" }}
+                    className="input-field"
+                    ref={inputRef}
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                  />
+                <button type="submit" style={{ margin: "5px 5px" }}>Submit</button>
+                </form>
+            </div>
+          </div>
+        </div>
+        )}
+      />
       <div className="app-container">
         <PatientInfo patientData={patientData} />
       </div>
