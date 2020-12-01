38b7f086c4bb2b13ebaed542690ff54d6a540bc0
ehayes9@gatech.edu <edxhayes@gmail.com>
2020-11-30 20:26:24 -0500

    coded FamilyMemberHistory GET using hapi.fhir.dstu3

--- app/api/api.py
+++ app/api/api.py
@@ -25,6 +25,8 @@ from fhirclient.models.codeableconcept import CodeableConcept
 from fhirclient.models.fhirsearch import FHIRSearchParam
 from fhirclient.models.fhirabstractbase import FHIRValidationError
 from fhirclient.models.observation import Observation
+from fhirclient.models.familymemberhistory import FamilyMemberHistory
+
 from requests.exceptions import HTTPError
 from datetime import datetime
 
@@ -43,6 +45,7 @@ PEDIATRICS_AGE_LIMIT = (
 smart_defaults = {
     "app_id": "pre_populated_intake_froms",
     "api_base": "https://apps.hdap.gatech.edu/syntheticmass/baseDstu3",
+    # "api_base": "https://hapi.fhir.org/baseDstu3",
 }
 conditions_list = {
     "38341003": "HyperTension",
@@ -426,6 +429,65 @@ def getHealthHabitsForPatient(id):
 
 
 # TODO Get Family Medical History
+
+# requires "api_base": "https://hapi.fhir.org/baseDstu3",
+# patient id:  2743900
+# returns a prostate cancer history item
+@app.route('/api/family_member_history/<id>', methods=['GET'])
+def get_family_member_history_for_patient(id):
+    smart = _get_smart()
+    """
+    Get Family Member History Observations by patient id
+    """
+
+    # 1e19bb7a-d990-4924-9fae-be84f19c53c1
+    try:
+        results = []
+        p_search = FamilyMemberHistory.where(struct={'patient': "Patient/" + str(id)})
+        p_history = p_search.perform_resources(smart.server)
+        print(id)
+        print(len(p_history))
+
+        for hist in p_history:
+            brief_display = ''
+            relationship = ''
+            if hist.relationship:
+                relationship = hist.relationship.coding[0].display.lower()
+            code = ''
+            display = ''
+            hist_id = ''
+            system = ''
+            if hist.condition:
+                code = hist.condition[0].code.coding[0].code
+                display = hist.condition[0].code.coding[0].display
+                system = hist.condition[0].code.coding[0].system
+            if hist.id:
+                hist_id = hist.id
+            if relationship not in ['mother', 'father', 'sister', 'brother']:
+                relationship = ''
+
+            for form_condition in ['cancer', 'heart', 'diabetes']:
+                if 'cancer' in form_condition:
+                    brief_display = form_condition
+
+            if relationship != '' and brief_display != '':
+                brief = {"brief_display" : brief_display, "relationship" : relationship }
+                details = {"code": code, "display": display, "id": hist_id, "system": system}
+                results.append({"brief": brief, "details": details})
+
+        # results.sort(key=lambda m: m.get("display"))
+        return jsonify(results)
+
+    except FHIRValidationError:
+        # The server should probably return a more adequate HTTP error code here instead of a 200 OK.
+        return jsonify({'error': 'sorry, we\' querying a public server and someone must have entered something \
+                                            not valid there'})
+    except HTTPError:
+        # Same as the error handler above. This is a bad pattern. Should return a HTTP 5xx error instead.
+        return jsonify({'error': 'something really bad has happened!'})
+
+
+
 # TODO get surgical history
 @app.route("/api/Procedure/<id>", methods=["GET"])
 def getSurgicalHistoryForPatient(id):
@@ -469,8 +531,8 @@ def getSurgicalHistoryForPatient(id):
 
 
 # TODO post surgical history
-@app.route("/api/Procedure/<id>", methods==["PUT"])
-def update_SurgicalHistoryForPatient(id):
+# @app.route("/api/Procedure/<id>", methods==["PUT"])
+# def update_SurgicalHistoryForPatient(id):
 # TODO post surgical history
 #needs to add in fields for this unsure of how
 
